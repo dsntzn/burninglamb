ca48a88cd1b54ae3f543d2e0a6ba8999a6d3da26
Dušan Tužinský <tuzinsky@lbstudio.sk>
2026-02-05 19:50:28 +0100

    updates

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,8 +15,8 @@ export default function Project({
       className="group relative rounded-xl bg-[var(--bg-background-rgb)]"
       {...other}
     >
-      <div className="grid grid-cols-[auto_1fr] md:grid-cols-[auto_1fr_auto] items-center bg-[var(--background-rgb)] gap-3 md:gap-5 lg:gap-14 sticky py-3 top-[100px] md:top-[48.5px] z-[9] border-b-0.5">
-        <ProjectsTitle className="hidden md:flex py-0 opacity-0" aria-hidden/>
+      <div className="grid grid-cols-[auto_1fr] md:grid-cols-[auto_1fr_auto] items-center bg-[var(--background-rgb)] gap-3 md:gap-5 lg:gap-10 sticky py-3 top-[100px] md:top-[48.5px] z-[9] border-b-0.5">
+        <ProjectsTitle className="hidden md:flex py-0 opacity-0 md:mr-10" aria-hidden/>
         <h3 className="flex items-center gap-2 bg-transparent text-3xl font-semibold leading-0.9 md:text-4xl">
           <span
             className="rounded-full border border-primary-500 px-2 py-1 text-xs font-bold leading-1.3 tracking-2 text-primary-500 transition-colors duration-500 group-open:bg-primary-500 group-open:text-black group-hover:bg-primary-500 group-hover:text-black"
@@ -26,7 +26,7 @@ export default function Project({
           </span>
           <span className="text-base leading-1.2">{title}</span>
         </h3>
-        <div className="hidden md:flex flex-wrap gap-1 justify-end">
+        <div className="hidden sm:flex flex-wrap gap-1 justify-end">
           {tags.map((tag) => (
             <Button key={tag} isAlwaysSmall>
               {tag}
