f49ae7ab530fd548d17c3e234633ddbb353c8bdf
Dušan Tužinský <tuzinsky@lbstudio.sk>
2026-02-05 19:32:59 +0100

    fix big button

--- src/components/BigButton.js
+++ src/components/BigButton.js
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 const Arrow = ({ className }) => (
   <div className={twMerge(className)}>
     <svg width="102" height="58" viewBox="0 0 51 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path className="arrow-line origin-left duration-500 scale-x-[66%] group-hover:scale-x-100 group-hover:stroke-white transition-all" d="M49.5137 14.3005L0 14.5005" stroke="#000" stroke-width="2.63889" stroke-linecap="square" strokelinejoin="round" />
+      <path className="arrow-line origin-left duration-500 scale-x-[66%] group-hover:scale-x-100 group-hover:stroke-white transition-all" d="M49.5137 14.3005L0 14.5005" stroke="#000" stroke-width="2.63889" stroke-linecap="square" stroke-linejoin="round" />
       <path className="arrow-head origin-left duration-500 -translate-x-1/3 group-hover:translate-x-0 group-hover:stroke-white transition-all" d="M35.0545 26.8875C41.3581 17.8033 49.6162 14.5 49.6162 14.5C49.6162 14.5 41.3581 11.1967 35.0544 2.11184" stroke="#000" stroke-width="2.63889" stroke-linecap="square" stroke-linejoin="round" />
     </svg>
   </div>
